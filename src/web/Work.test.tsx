397ec11ad8310cf5ba2b3326e04267c620172a6d
kassol <kassol.zx@gmail.com>
2026-09-19 00:06:39 +0800

    Render report tables with GFM

---
+++ src/web/Work.test.tsx
@@ -0,0 +1,15 @@
+import { expect, test } from 'bun:test'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { ReportMarkdown } from './Work'
+
+test('report renders a GFM table without unsafe links, HTML or images', () => {
+  const markdown = '| 参数 | 用途 |\n|---|---|\n| `q` | 搜索词 |\n\n[safe](https://example.com/) [unsafe](javascript:alert(1))\n\n<script>alert(1)</script>\n\n![image](https://example.com/image.png)'
+  const html = renderToStaticMarkup(<ReportMarkdown markdown={markdown} />)
+  expect(html).toContain('<table>')
+  expect(html).toContain('<th>参数</th>')
+  expect(html).toContain('<td><code>q</code></td>')
+  expect(html).toContain('href="https://example.com/"')
+  expect(html).not.toContain('href="javascript:')
+  expect(html).not.toContain('<script')
+  expect(html).not.toContain('<img')
+})
